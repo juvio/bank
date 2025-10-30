1e79e56df90d6e70a4a99f3931ff023802027ec7
juvio <julianavieirao.ppg@live.com>
2025-10-30 14:27:24 -0300

    feat: enhance ModalComponent with customizable title, content, and actions and styles

--- src/app/(operations)/@modal/(.)transaction/page.tsx
+++ src/app/(operations)/@modal/(.)transaction/page.tsx
@@ -1,5 +1,12 @@
 import ModalComponent from '@/components/Modal';
 
 export default function TransactionModal() {
-  return <ModalComponent />;
+  return (
+    <ModalComponent 
+      title="Confirmar Transação"
+      content="Deseja realmente criar esta transação? Esta ação não poderá ser desfeita."
+      confirmText="Criar Transação"
+      cancelText="Cancelar"
+    />
+  );
 }
