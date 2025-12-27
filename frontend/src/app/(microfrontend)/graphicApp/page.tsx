54b02e91416f6f3c1a683fd1ae73f0eb5f3c947b
carolly <carolly.barbosa@gmail.com>
2025-12-27 01:23:36 -0300

    chore: teste com iframe

--- frontend/src/app/(microfrontend)/graphicApp/page.tsx
+++ frontend/src/app/(microfrontend)/graphicApp/page.tsx
@@ -1,7 +1,8 @@
 export default async function MicrofrontendPage() {
-  const html = await fetch('http://localhost:3001', {
-    next: { revalidate: 0 },
-  }).then((res) => res.text());
-
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  return (
+    <iframe
+      src='http://localhost:3001'
+      style={{ width: '100%', height: '75vw' }}
+    />
+  );
 }
