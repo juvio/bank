a1d05148bc1e50ed89e4104357ae6c95b781b777
carolly <carolly.barbosa@gmail.com>
2025-12-30 13:06:30 -0300

    chore: adiciona mock na home

--- frontend/src/app/(operations)/home/page.tsx
+++ frontend/src/app/(operations)/home/page.tsx
@@ -3,17 +3,22 @@ import HomePageComponent from '@/components/HomePageComponent';
 import { parse } from 'cookie';
 
 export default async function HomePage() {
-  const h = await headers();
-  const cookie = h.get('cookie');
-  const cookies = parse(cookie || '');
-  const token = cookies.token;
-  const data = await fetch('http://localhost:5000/user', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const response = await data.json();
-  const fullname = response.result[0].username;
+  const USE_MOCK = process.env.NEXT_PUBLIC_USE_MOCK === 'true';
+  let fullname = 'Usuário Teste';
 
-  return <HomePageComponent fullname={fullname} />;
+  if (!USE_MOCK) {
+    const h = await headers();
+    const cookie = h.get('cookie');
+    const cookies = parse(cookie || '');
+    const token = cookies.token;
+    const data = await fetch('http://localhost:5000/user', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const response = await data.json();
+    fullname = response.result[0].username;
+  }
+
+  return <HomePageComponent fullname={USE_MOCK ? 'Mock User' : fullname} />;
 }
